8f92db93e5641b1a6e5355ad0d6e32ec28a41f24
郑飞 <zhengfei@dianzhong.com>
2021-01-28 11:50:24 +0800

    add float32 convert

--- type_convert.go
+++ type_convert.go
@@ -48,3 +48,15 @@ func StrToFloat64(inp string, defaultValue float64) float64 {
 func Float64ToStr(inp float64) string {
 	return strconv.FormatFloat(inp, 'E', -1, 64)
 }
+
+func StrToFloat32(inp string, defaultValue float32) float32 {
+	val, err := strconv.ParseFloat(inp, 64)
+	if err != nil {
+		return defaultValue
+	}
+	return float32(val)
+}
+
+func Float32ToStr(inp float32) string {
+	return strconv.FormatFloat(float64(inp), 'E', -1, 64)
+}
